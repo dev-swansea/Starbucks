6d8b97e5f4e85a8e470efd604ffa90e413d67b62
suwan <suwansea2013@naver.com>
2022-07-29 22:37:33 +0900

    [KDC] React&Redux로 시작하는 웹프로그래밍_4주차_HTML/CSS/JS로 만드는 스타벅스 웹사이트_스타벅스 예제(4)(1:42:45) 끝내고

--- js/main.js
+++ js/main.js
@@ -15,21 +15,70 @@ searchInputEl.addEventListener("blur", () => {
   searchInputEl.setAttribute("placeholder", "");
 });
 
-// 클래스 선탁자는 웬만하면 부모 요소까지 적는구나 ?
-const bedgeEl = document.querySelector("header .bedges");
-// 윈도우는 브라우저창(보고있는 화면 자체?) 이라고 보면 된다. 윈도우 객체는 브라우저가 가지고 있는 명령들을 가지고 있다.
-// scroll할때마다 이벤트가 발생한다.=>페이지가 무거워질 수 있다. 화면 버벅임이 생길 수 있기 때문에 라이브러리를 이용해 scroll함수를 제어할 것이다. throttle은 cdn으로 연결한 라이브러리 메소드를 사용하는 것이다., 이건 제어 시간을 줘서 한 번씩만 실행되도록 하는 것이다.
+const badgeEl = document.querySelector("header .badges");
 window.addEventListener(
   "scroll",
   _.throttle(() => {
     if (window.scrollY > 500) {
-      // 벳지 숨기기
-      // gsap.to(요소, 지속시간, 옵션)
-      gsap.to(bedgeEl, 0.6, { opacity: 0, display: "none" });
+      gsap.to(badgeEl, 0.6, { opacity: 0, display: "none" });
     } else {
-      // 벳지 보이기
-      gsap.to(bedgeEl, 0.6, { opacity: 1, display: "block" });
+      gsap.to(badgeEl, 0.6, { opacity: 1, display: "block" });
     }
   }, 300)
 );
-//_.throttle(함수, 시간)
+
+// animation
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+// 두 번째 매개변수는 반복되는 횟수를 index라는 이름으로 사용이 가능했다고요?
+fadeEls.forEach((fadeEl, index) => {
+  // gsap.to(요소, 작업시간(초 단위), 옵션{객체데이터}), delay는 순차적으로 보여주기 위해 사용하는 메소드
+  // delay:0.7은 0.7초 이후 보여지게 하는 것인데 둘 다 묶여있으므로 순차적 효과가 나타나지 않는다 그래서 (index + 1) *를 해준다.
+  gsap.to(fadeEl, 1, { opacity: 1, delay: (index + 1) * 0.7 }); //0.7, 1.4, 2.1, 2.7, 이렇게 하니까 굳이 작업시간을 명시해줄 필요가 없는데?
+});
+
+// notice slide
+// js 생성자(클래스) 함수에 선택자를 인수로 삽입해준다, 두 번째 인수로 옵션{object}을 넣어준다.
+new Swiper(".notice-line .swiper-container", {
+  direction: "vertical",
+  // 자동
+  autoplay: true,
+  // 반복
+  loop: true,
+});
+
+// Promotion slide
+new Swiper(".promotion .swiper-container", {
+  // direction:horizontal default값으로 따로 명시할 필요가 없다(수직 이동)
+  slidesPerView: 3, //한번에 보여줄 슬라이드 갯수
+  spaceBetween: 10, //슬라이드 사이 여백
+  centeredSlides: true, //1번 슬라이드가 가운데 보이게 하기
+  loop: true,
+  // autoplay: {
+  //   delay: 5000, //5초에 한 번씩 자동으로 슬라이드, 기본값은 3초
+  // },
+  pagination: {
+    el: ".promotion .swiper-pagination", //페이지 요소 번호 매기기
+    clickable: true,
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
+});
+
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener("click", () => {
+  // !로 반대의 값을 할당한다. true로만 하면 안되지, 또 다시 눌렀을 때도 계속 true가 되니까
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    // 숨김 처리
+    promotionEl.classList.add("hide");
+  } else {
+    // 보임 처리
+    promotionEl.classList.remove("hide");
+  }
+});
